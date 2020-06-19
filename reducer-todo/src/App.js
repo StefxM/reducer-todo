7e0e53831c48beedf13fb15a0ccb8cb1547ca2e9
stephanie mairena <stefyem7711@gmail.com>
2020-06-19 00:52:37 -0400

    stuck

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,6 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+const todoTasks = [
+  {
+    task: 'Organize Garage', // title
+    id: 1528817077286, //time stamp - `Date.now()`
+    completed: false//completed toggle
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  },
+  {
+      task: 'Wash Dishes',
+      id: 1528817084361,
+      completed: false
+  },
+  {
+      task: 'Laundry',
+      id: 1528817084364,
+      completed: false
+  },
+  {
+      task: 'Meal Prep',
+      id: 1528817084366,
+      completed: false
+  },
+  {
+      task: 'Set up App',
+      id: 1528817084369,
+      completed: false
+  }
+];
+
+
+
 
 function App() {
   return (
---
+++ reducer-todo/src/Components/TodoForm.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const TodoForm = () => {
+
+    return(
+        <div>
+            <form>
+                <input 
+                type="text"
+                name="todo"
+                value={todo}
+                //onChange=
+                />
+                <button placeholder=""></button>
+            </form>
+        </div>
+
+    )
+}
+
+export default TodoForm;
